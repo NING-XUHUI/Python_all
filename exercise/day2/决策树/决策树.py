c87837ab7484fa6085afd378fee3e3987a3b98d9
NING-XUHUI <892897986@qq.com>
2020-04-02 22:14:49 +0800

    py

--- exercise/day2/决策树/决策树.py
+++ exercise/day2/决策树/决策树.py
@@ -15,6 +15,14 @@ from sklearn.tree import DecisionTreeClassifier, export_graphviz
     回归树：
     分类树：基尼系数（划分更加细致）
     DecisionTree
+    
+决策树的优缺点：
+    优点：1-简单的理解和解释，树木可视化
+         2-需要很少的数据准备，其他技术通常需要数据归一化
+    缺点：1-决策树学习者可以创建不能很好地推广数据的过于复杂的树，这被称为过拟合。
+    改进：1-减枝cart算法（决策树API当中已经实现，随机森林参数调优有关介绍）
+         2-随机树林
+    注意：企业重要决策，由于决策树很好的分析能力，在决策过程应用较多
 """
 
 
@@ -60,7 +68,7 @@ def decision():
     # print(x_train,y_train)
     print(dec.score(x_train, y_train))
 
-    export_graphviz(dec, out_file="./tree.dot", feature_names=['Age', 'Pclass', 'Sex=female', 'Sex=male'])
+    # export_graphviz(dec, out_file="./tree.dot", feature_names=['Age', 'Pclass', 'Sex=female', 'Sex=male'])
     return None
 
 
